2fc0ebf593a594b58d0e9d2efd6585c83c5ca8e9
shiv <sk.sk.shivukumar@gmail.com>
2025-07-31 19:15:05 +0530

    app created

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', // Add Prettier integration
+  ],
+  rules: {
+    // Your custom rules here
+  },
+};
