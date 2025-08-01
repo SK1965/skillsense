6f4e307fe8b663b58219f08f60fa66608e9bf528
shiv <sk.sk.shivukumar@gmail.com>
2025-08-01 17:04:15 +0530

    form

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,9 @@ export const Navbar = () => {
           SkillSense
         </h1>
       </div>
-      <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+      <button
+      onClick={() => alert('Login functionality coming soon!')} 
+      className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
       </button>
     </nav>
