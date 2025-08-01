5e426ca70e48d11a0aad7be7ef9491a66636aa59
shiv <sk.sk.shivukumar@gmail.com>
2025-08-01 14:30:36 +0530

    handled pdf-parse isue

--- src/app/api/analyze/route.ts
+++ src/app/api/analyze/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import pdf from 'pdf-parse';
+import pdf from 'pdf-parse/lib/pdf-parse.js';
 import generate from './generate';
 
 export async function POST(req: NextRequest) {
